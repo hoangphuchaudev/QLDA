2036f7f1b903ea6d4b9abea2084469e2b0d36210
Hau Hau <dauphainguoitot@gmail.com>
2022-08-22 16:59:58 +0700

    Menu

--- js/menu-all-product.js
+++ js/menu-all-product.js
@@ -148,8 +148,8 @@ var html='<div class="menu-all-product">'+
 "</div>";return html;},renderHTMLMenuAllProductForMobile:function(){var follow=''
 if(jQuery('.is-misa').length==0){follow='rel="nofollow"';}
 var html="";html=html.concat('<select class="h-option-select h-option-select-product">'+
-'<option value="" selected disabled hidden>Tất cả sản phẩm MISA</option>'+
-'<optgroup label="Cơ quan Nhà nước">'+
+'<option value="" selected disabled hidden>NTSOFT</option>'+
+'<optgroup label="Khối cơ quan Nhà nước">'+
 '<option value="https://bumas.misa.vn/" '+follow+'>MISA Bumas - Quản lý ngân sách</option>'+
 '<option value="https://mimosa.misa.vn/" '+follow+'>MISA Mimosa 2022 - Kế toán HCSN</option>'+
 '<option value="https://bamboo.misa.vn/" '+follow+'>MISA Bamboo.NET 2020 - Kế toán xã/phường</option>'+
@@ -166,44 +166,9 @@ var html="";html=html.concat('<select class="h-option-select h-option-select-pro
 '<option value="http://eoffice.misa.vn/taskgo/">MISA TaskGo - Quản lý công việc</option>'+
 '<option value="http://eoffice.misa.vn/cegov/">MISA CeGov - Quản lý Thi Đua Khen Thưởng</option>'+
 "</optgroup>"+
-'<optgroup label="Doanh nghiệp">'+
+'<optgroup label="Khối doanh nghiệp">'+
 '<option value="https://sme.misa.vn/" '+follow+'>MISA SME 2022 - Quản lý kế toán (offline)</option>'+
-'<option value="https://amis.misa.vn/amis-ke-toan/" '+follow+'>AMIS Kế toán</option>'+
-'<option value="https://www.meinvoice.vn/" '+follow+'>MISA meInvoice - Hóa đơn điện tử</option>'+
-'<option value="https://asp.misa.vn/" '+follow+'>MISA ASP - Kế toán dịch vụ</option>'+
-'<option value="https://lending.misa.vn/" '+follow+'>MISA Lending - Kết nối vay vốn doanh nghiệp</option>'+
-'<option value="https://mtax.misa.vn/" '+follow+'>MISA mTax - Kê khai, nộp tờ khai thuế</option>'+
-'<option value="https://esign.misa.vn/" '+follow+'>MISA eSign - Chữ ký số điện tử</option>'+
-'<option value="https://bankhub.misa.vn/" '+follow+'>MISA BankHub - Ngân hàng điện tử</option>'+
-'<option value="https://amis.misa.vn/phan-mem-crm-amis/" '+follow+'>AMIS Bán hàng - Quản lý bán hàng</option>'+
-'<option value="https://amis.misa.vn/amis-khuyen-mai/" '+follow+'>AMIS Khuyến mại - Quản lý khuyến mại</option>'+
-'<option value="https://jetpay.vn/jetpay-payment/" '+follow+'>JetPay Payment - Cổng thanh toán</option>'+
-'<option value="https://amis.misa.vn/amis-aimarketing/" '+follow+'>AMIS aiMarketing - Công cụ Automation Marketing</option>'+
-'<option value="https://amis.misa.vn/amis-tuyen-dung/" '+follow+'>AMIS Tuyển dụng - Quản lý tuyển dụng</option>'+
-'<option value="https://amis.misa.vn/thong-tin-nhan-su/" '+follow+'>AMIS Thông tin nhân sự - Hệ thống thông tin nhân sự</option>'+
-'<option value="https://amis.misa.vn/phan-mem-cham-cong/" '+follow+'>AMIS Chấm công - Quản lý chấm công</option>'+
-'<option value="https://amis.misa.vn/tien-luong/" '+follow+'>AMIS Tiền lương - Tính toán và quản lý tiền lương</option>'+
-'<option value="">AMIS Nhân viên - Coming soon 2021</option>'+
-'<option value="https://amis.misa.vn/phan-mem-bao-hiem-xa-hoi/" '+follow+'>AMIS BHXH - Bảo hiểm xã hội điện tử</option>'+
-'<option value="https://amis.misa.vn/amis-cong-viec/" '+follow+'>AMIS Công việc - Quản lý công việc</option>'+
-'<option value="https://amis.misa.vn/amis-ghi-chep/" '+follow+'>AMIS Ghi chép - Ghi chép & lưu trữ tài liệu</option>'+
-'<option value="https://amis.misa.vn/amis-wesign/" '+follow+'>AMIS WeSign - Ký tài liệu số</option>'+
-'<option value="https://amis.misa.vn/amis-mang-xa-hoi-doanh-nghiep/" '+follow+'>AMIS Mạng xã hội - Mạng xã hội doanh nghiệp</option>'+
-'<option value="https://amis.misa.vn/amis-tai-san/" '+follow+'>AMIS tài sản - Quản lý tài sản</option>'+
-'<option value="https://amis.misa.vn/amis-phong-hop/" '+follow+'>AMIS Phòng họp - Quản lý phòng họp</option>'+
-'<option value="https://amis.misa.vn/amis-nhan-su/" '+follow+'>AMIS Nhân sự - Quản lý nhân sự</option>'+
 "</optgroup>"+
-'<optgroup label="Giải pháp bán lẻ">'+
-'<option value="https://www.cukcuk.vn/" '+follow+'>MISA Cukcuk - Quản lý nhà hàng</option>'+
-'<option value="https://www.cukcuk.vn/misa-lomas/" '+follow+'>MISA Lomas - Nghiệp vụ chăm sóc khách hàng</option>'+
-'<option value="https://www.cukcuk.vn/ban-hang-online/" '+follow+'>Bán hàng Online - Xây dựng Website bán hàng chuyên nghiệp</option>'+
-'<option value="https://www.cukcuk.vn/dang-ky-grabfood-cung-cukcuk/" '+follow+'>Kết nối GrabFood - Kết nối và đồng bộ đơn hàng từ GrabFood</option>'+
-'<option value="https://eshop.misa.vn/" '+follow+'>MISA eShop - Quản lý cửa hàng</option>'+
-'<option value="https://eshop.misa.vn/san-pham-ocm/" '+follow+'>MISA OCM - Nghiệp vụ Bán hàng online</option>'+
-'<option value="https://eshop.misa.vn/misa-lomas/" '+follow+'>MISA Lomas - Nghiệp vụ chăm sóc khách hàng</option>'+
-'<option value="https://moneykeeper.misa.vn/" '+follow+'>MISA MoneyKeeper - Quản lý tài chính cá nhân</option>'+
-'<option value="https://golf.misa.vn/" '+follow+'>MISA Golf - Nền tảng cho Golfer</option>'+
-"/optgroup>"+
 "</select>");return html;},};function menuAllProductCSS(){if(!document.getElementById("css")){var head=document.getElementsByTagName("head")[0];var link=document.createElement("link");link.id="css";link.rel="stylesheet";link.type="text/css";link.href=misa_site+"/sites/menu/menu-all-product.css?ver=8";link.media="all";head.appendChild(link);}};var prevScrollpos=window.pageYOffset;window.onscroll=function(){if(jQuery(".header-top-custom").length>0){if(window.innerWidth>=980){if(!jQuery(".header-top-custom").is(":hover")){if(!jQuery(".td-header-menu-wrap").is(":hover")){var currentScrollPos=window.pageYOffset;var heightMenu=jQuery(".header-top-custom")[0].clientHeight;if(prevScrollpos>currentScrollPos){document.getElementsByClassName("td-header-menu-wrap-full")[0].style.top="0";}
 else{if(currentScrollPos>heightMenu){var heightMenuProductPC=document.getElementById("custom-menu-item").clientHeight;if(heightMenuProductPC<=0){document.getElementsByClassName("td-header-menu-wrap-full")[0].style.top="-"+(heightMenu+1)+"px";}}}
 prevScrollpos=currentScrollPos;}}}else{var currentScrollPos=window.pageYOffset;var heightMenu=jQuery(".header-top-custom")[0].clientHeight;if(prevScrollpos>currentScrollPos){document.getElementsByClassName("td-header-menu-wrap-full")[0].style.top="0";}

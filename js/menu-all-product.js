adb326e1a2a728de1bbf54afd66475072100d455
Hau Hau <dauphainguoitot@gmail.com>
2022-08-23 09:35:43 +0700

    Custom 23/08/2022

--- js/menu-all-product.js
+++ js/menu-all-product.js
@@ -147,29 +147,15 @@ var html='<div class="menu-all-product">'+
 "</div>"+
 "</div>";return html;},renderHTMLMenuAllProductForMobile:function(){var follow=''
 if(jQuery('.is-misa').length==0){follow='rel="nofollow"';}
-var html="";html=html.concat('<select class="h-option-select h-option-select-product">'+
-'<option value="" selected disabled hidden>NTSOFT</option>'+
+var html="";html=html.concat('<li id="menu-item-29" class="menu-item-has-children menu-item menu-item-type-custom menu-item-object-custom menu-item-29"><select class="h-option-select h-option-select-product">'+
+'<option value="" selected class="menu-item-has-children menu-item menu-item-type-custom menu-item-object-custom menu-item-29" disabled hidden>NTSOFT</option>'+
 '<optgroup label="Khối cơ quan Nhà nước">'+
-'<option value="https://bumas.misa.vn/" '+follow+'>MISA Bumas - Quản lý ngân sách</option>'+
-'<option value="https://mimosa.misa.vn/" '+follow+'>MISA Mimosa 2022 - Kế toán HCSN</option>'+
-'<option value="https://bamboo.misa.vn/" '+follow+'>MISA Bamboo.NET 2020 - Kế toán xã/phường</option>'+
-'<option value="https://qlts.misa.vn/" '+follow+'>MISA QLTS - Quản lý tài sản</option>'+
-'<option value="https://salagov.misa.vn/" '+follow+'>MISA SalaGov - Quản lý tiền lương</option>'+
-'<option value="https://panda.misa.vn/" '+follow+'>MISA Panda.NET 2021 - Kế toán Thi hành án dân sự </option>'+
-'<option value="https://falcon.misa.vn/" '+follow+'>MISA Falcon - Kế toán Quỹ "Vì người nghèo"</option>'+
-'<option value="https://lekima.misa.vn/" '+follow+'>MISA Lekima - Quyết toán tài chính</option>'+
-'<option value="https://emis.misa.vn/" '+follow+'>MISA EMIS - Quản lý trường học</option>'+
-'<option value="https://sisap.misa.vn/" '+follow+'>MISA SISAP - Chia sẻ thông tin Nhà trường – Phụ huynh</option>'+
-'<option value="https://jetpay.vn/jetpay-tuition/" '+follow+'>JetPay Tuition - Thu tiền học trực tuyến</option>'+
-'<option value="https://qlcb.misa.vn/" '+follow+'>MISA QLCB - Quản lý Cán bộ - Công chức, Viên chức</option>'+
-'<option value="https://hotich.misa.vn/" '+follow+'>MISA Hotich - Đăng ký và Quản lý hộ tịch</option>'+
-'<option value="http://eoffice.misa.vn/taskgo/">MISA TaskGo - Quản lý công việc</option>'+
-'<option value="http://eoffice.misa.vn/cegov/">MISA CeGov - Quản lý Thi Đua Khen Thưởng</option>'+
+'<option value="https://bumas.misa.vn/" '+follow+'>Phần mềm Quản lý dự án</option>'+
 "</optgroup>"+
 '<optgroup label="Khối doanh nghiệp">'+
-'<option value="https://sme.misa.vn/" '+follow+'>MISA SME 2022 - Quản lý kế toán (offline)</option>'+
+'<option value="https://sme.misa.vn/" '+follow+'>Phần mềm Quản lý nhuận bút</option>'+
 "</optgroup>"+
-"</select>");return html;},};function menuAllProductCSS(){if(!document.getElementById("css")){var head=document.getElementsByTagName("head")[0];var link=document.createElement("link");link.id="css";link.rel="stylesheet";link.type="text/css";link.href=misa_site+"/sites/menu/menu-all-product.css?ver=8";link.media="all";head.appendChild(link);}};var prevScrollpos=window.pageYOffset;window.onscroll=function(){if(jQuery(".header-top-custom").length>0){if(window.innerWidth>=980){if(!jQuery(".header-top-custom").is(":hover")){if(!jQuery(".td-header-menu-wrap").is(":hover")){var currentScrollPos=window.pageYOffset;var heightMenu=jQuery(".header-top-custom")[0].clientHeight;if(prevScrollpos>currentScrollPos){document.getElementsByClassName("td-header-menu-wrap-full")[0].style.top="0";}
+"</select></li>");return html;},};function menuAllProductCSS(){if(!document.getElementById("css")){var head=document.getElementsByTagName("head")[0];var link=document.createElement("link");link.id="css";link.rel="stylesheet";link.type="text/css";link.href=misa_site+"/sites/menu/menu-all-product.css?ver=8";link.media="all";head.appendChild(link);}};var prevScrollpos=window.pageYOffset;window.onscroll=function(){if(jQuery(".header-top-custom").length>0){if(window.innerWidth>=980){if(!jQuery(".header-top-custom").is(":hover")){if(!jQuery(".td-header-menu-wrap").is(":hover")){var currentScrollPos=window.pageYOffset;var heightMenu=jQuery(".header-top-custom")[0].clientHeight;if(prevScrollpos>currentScrollPos){document.getElementsByClassName("td-header-menu-wrap-full")[0].style.top="0";}
 else{if(currentScrollPos>heightMenu){var heightMenuProductPC=document.getElementById("custom-menu-item").clientHeight;if(heightMenuProductPC<=0){document.getElementsByClassName("td-header-menu-wrap-full")[0].style.top="-"+(heightMenu+1)+"px";}}}
 prevScrollpos=currentScrollPos;}}}else{var currentScrollPos=window.pageYOffset;var heightMenu=jQuery(".header-top-custom")[0].clientHeight;if(prevScrollpos>currentScrollPos){document.getElementsByClassName("td-header-menu-wrap-full")[0].style.top="0";}
 else{if(currentScrollPos>heightMenu){document.getElementsByClassName("td-header-menu-wrap-full")[0].style.top="-"+(heightMenu+1)+"px";}}
